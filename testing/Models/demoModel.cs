69900b5a6924adf005c05d974d5f156ba6635e7b
Jahidul-Islam-Rakib <jahidul.irk@gmail.com>
2024-10-22 00:44:48 +0600

    multiple property add korsi

--- testing/Models/demoModel.cs
+++ testing/Models/demoModel.cs
@@ -4,5 +4,11 @@
 	{
         public int MyProperty { get; set; }
 	public string check1{get; set;}
-    }
+		public int MyProperty { get; set; }
+		public required string check1 { get; set; }
+		public int MyProperty { get; set; }
+		public string check1 { get; set; }
+		public int MyProperty { get; set; }
+		public string check1 { get; set; }
+	}
 }
