068aed36f69bb0693998e310ae36ac7f95826037
Md. Jahidul Islam Rakib <168633979+Jahidul-islam-rakib@users.noreply.github.com>
2024-10-22 00:45:44 +0600

    multile property delete korsi

--- testing/Models/demoModel.cs
+++ testing/Models/demoModel.cs
@@ -4,11 +4,6 @@
 	{
         public int MyProperty { get; set; }
 	public string check1{get; set;}
-		public int MyProperty { get; set; }
-		public required string check1 { get; set; }
-		public int MyProperty { get; set; }
-		public string check1 { get; set; }
-		public int MyProperty { get; set; }
-		public string check1 { get; set; }
+		
 	}
 }
